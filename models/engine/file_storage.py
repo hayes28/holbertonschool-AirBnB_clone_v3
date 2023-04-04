746a25baf183f3d02fd33aab24d83693cd07badf
HH <heather.hayes918@gmail.com>
2023-04-04 17:12:56 -0500

    Updated db and file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
 """
-Contains the FileStorage class
+This module defines the FileStorage class for serializing and deserializing
+objects to and from a JSON file.
+
+This file contains the FileStorage class, which is responsible for saving and
+loading objects to and from a JSON file. It defines methods for saving objects
+to the file, loading objects from the file, and retrieving objects by class
+and ID. It also defines methods for deleting objects and counting the number
+of objects stored in the file.
 """
 
 import json
@@ -12,20 +18,32 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
+# Define a dictionary mapping class names to their corresponding model classes
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
+    """Serializes and deserializes objects to and from a JSON file.
 
-    # string - path to the JSON file
+    This class is responsible for saving and loading objects to and from a JSON
+    file. It defines methods for saving objects to the file, loading objects
+    from the file, retrieving objects by class and ID, deleting objects, and
+    counting the number of objects stored in the file.
+    """
+    # The path to the JSON file where objects are stored
     __file_path = "file.json"
-    # dictionary - empty but will store all objects by <class name>.id
+    # A dictionary mapping object IDs to the corresponding objects
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """Returns a dictionary of all objects stored in the file.
+
+        This method returns a dictionary mapping object IDs to the corresponding
+        objects for all objects stored in the file. If a class is specified,
+        it returns a dictionary of all objects of that class only.
+        """
         if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
@@ -35,13 +53,23 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """Adds a new object to the dictionary of objects stored in the file.
+
+        This method adds a new object to the dictionary of objects stored in the
+        file. It uses the object's class name and ID to create a key for the
+        dictionary entry.
+        """
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """Serializes all objects to the JSON file.
+
+        This method serializes all objects in the dictionary of objects stored
+        in the file to a JSON-formatted string and writes the string to the file
+        specified by __file_path.
+        """
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
@@ -49,7 +77,12 @@ class FileStorage:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """Deserializes all objects from the JSON file.
+
+        This method reads the JSON-formatted string from the file specified by
+        __file_path and deserializes the objects back into Python objects. It
+        stores the objects in the dictionary of objects stored in the file.
+        """
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
@@ -68,3 +101,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID"""
+        if cls not in classes.values():
+            return None
+
+        list_obj = self.all(cls).values()
+        for obj in list_obj:
+            if obj.id == id:
+                return obj
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage"""
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
